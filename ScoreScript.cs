e30ef7065c6b01896b8c58017ce09d897a4c5027
germcauley <geraldmcauley@gmail.com>
2020-10-19 23:39:44 +0100

    scorescript added

---
+++ ScoreScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+
+    private Text coinTextScore;
+    private int scoreCount;
+
+    void Start()
+    {
+        coinTextScore = GameObject.Find("CoinText").GetComponent<Text>();
+      
+    }
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.gameObject.tag == MyTags.COIN_TAG)
+        {
+            print("cointouch!");
+            scoreCount++;
+            coinTextScore.text = "x" + scoreCount;
+        }
+    }
+
+}//class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
