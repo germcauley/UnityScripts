1d00e3b9a64b978960437a7579a3e2257e8d4690
germcauley <geraldmcauley@gmail.com>
2020-11-02 20:09:40 +0000

    player attack

--- PlayerMovementScript.cs
+++ PlayerMovementScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class GerPlayerMovement : MonoBehaviour
 {
 
     public float speed = 5f;
@@ -17,15 +17,22 @@ public class PlayerMovement : MonoBehaviour
 
     private bool isGrounded;
     private bool jumped;
+    
 
     public float jumpPower = 5f;
+    public AudioClip jumpClip;
+    public AudioClip landClip;
+    public AudioClip walkclip;
+
+
     AudioSource playerAudioData;
 
     void Awake()
     {
         myBody = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-        
+        playerAudioData = GetComponent<AudioSource>();
+
     }
 
 
@@ -34,50 +41,65 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        playerAudioData = GetComponent<AudioSource>();
+       
         Debug.Log("started");
     }
 
     // Update is called once per frame
+
     void Update()
     {
         CheckIfGrounded();
         PlayerJump();
+        PlayerWalk();
+        PlayerAttack();
+        PlayerSounds();
+        
+        
         if (Physics2D.Raycast(groundCheckPosition.position, Vector2.down, 0.5f, groundLayer))
         {
             //print("Collided with groud raycast");
         }
-    }
 
 
-     void FixedUpdate()
+     }
+   
+    void FixedUpdate()
     {
-        PlayerWalk();  
+        //PlayerWalk();
     }
 
     void PlayerWalk()
-    {
-        float h = Input.GetAxisRaw("Horizontal");
+    {       
 
+        float h = Input.GetAxisRaw("Horizontal");
         if (h > 0)
         {
+            
             myBody.velocity = new Vector2(speed, myBody.velocity.y);
-            ChangeDirection(1);
+            anim.SetBool("isRunning", true);
+            ChangeDirection(5);
+           
 
-        } else if (h < 0)
+        }
+        else if (h < 0)
         {
+            
             myBody.velocity = new Vector2(-speed, myBody.velocity.y);
+            anim.SetBool("isRunning", true);     
+            ChangeDirection(-5);
+            
 
-            ChangeDirection(-1);
         }
         else
         {
             myBody.velocity = new Vector2(0F, myBody.velocity.y);
+            anim.SetBool("isRunning", false);          
+            CancelInvoke();
+            
         }
 
         anim.SetInteger("Speed", Mathf.Abs((int)myBody.velocity.x));
-
-        
     }
 
     void ChangeDirection(int direction)
@@ -87,39 +109,84 @@ public class PlayerMovement : MonoBehaviour
         transform.localScale = tempScale;
     }
 
+   
 
-   void CheckIfGrounded()
+    void PlayerSounds()
+    {
+        if (isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
+            {             
+                playerAudioData.PlayOneShot(walkclip, 0.7F);
+            }   
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            playerAudioData.Stop();
+        }
+        
+    }
+
+
+    void CheckIfGrounded()
     {
         isGrounded = Physics2D.Raycast(groundCheckPosition.position, Vector2.down, 0.1f, groundLayer);
+        //print(isGrounded + "grounded!");
 
         if (isGrounded)
+            
         {
             if (jumped)
             {
                 jumped = false;
-
-                anim.SetBool("Jump", false);
+                anim.SetBool("Jump", false);  
+                playerAudioData.PlayOneShot(landClip, 0.5F);
+                print("Player land");
             }
         }
+       
     }
 
-   void PlayerJump()
+    void PlayerJump()
     {
         if (isGrounded)
         {
             if (Input.GetKey(KeyCode.Space))
             {
-                jumped = true;
                 myBody.velocity = new Vector2(myBody.velocity.x, jumpPower);
-
+                print("Player jump");
+                
+                playerAudioData.PlayOneShot(jumpClip, 0.5F);
                 anim.SetBool("Jump", true);
-                Console.WriteLine("Player jump");
-                playerAudioData.Play(0);
+
+                StartCoroutine(setJumped());
             }
         }
     }
 
+    void PlayerAttack()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            anim.SetBool("Attack", true);
+            print("PlayerAttack");          
+        }
+        if (Input.GetKeyUp(KeyCode.J))
+        {
+            anim.SetBool("Attack", false);
+            print("PlayerStopAttack");
+        }
+    }
 
+    IEnumerator setJumped()
+    {
+        yield return new WaitForSeconds(0.5f);
+        print("SET JUMP CO ROUTINE");
+        jumped = true;
+    }
+
+    
 }//class
 
 
