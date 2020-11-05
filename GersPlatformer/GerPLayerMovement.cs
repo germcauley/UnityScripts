029889b247a8296d08d55bc5f4b991734f09b8de
germcauley <geraldmcauley@gmail.com>
2020-11-05 23:46:51 +0000

    all player movement sounds added and scripted

--- GersPlatformer/GerPLayerMovement.cs
+++ GersPlatformer/GerPLayerMovement.cs
@@ -13,14 +13,14 @@ public class GerPlayerMovement : MonoBehaviour
 
 
     public Transform groundCheckPosition;
-    public LayerMask groundLayer,enemyLayer;
+    public LayerMask groundLayer, enemyLayer;
     //public Transform left_Collision, right_Collision, top_Collision, down_Collision;
     public Transform right_Collision;
     private Vector3 left_Collision_Pos, right_Collision_Pos;
 
     private bool isGrounded;
     private bool jumped;
-    
+    private bool fall,walking = false;
 
     public float jumpPower = 5f;
     public AudioClip jumpClip;
@@ -39,13 +39,10 @@ public class GerPlayerMovement : MonoBehaviour
 
     }
 
-
-
-
     // Start is called before the first frame update
     void Start()
     {
-       
+
         Debug.Log("started");
     }
 
@@ -55,52 +52,62 @@ public class GerPlayerMovement : MonoBehaviour
     {
         CheckIfGrounded();
         PlayerJump();
-        PlayerWalk();
         PlayerAttack();
+        PlayerWalk();
         PlayerSounds();
-        
-        
+
+
         if (Physics2D.Raycast(groundCheckPosition.position, Vector2.down, 0.5f, groundLayer))
         {
             //print("Collided with groud raycast");
         }
 
+    }
 
-     }
-   
     void FixedUpdate()
     {
-        //PlayerWalk();
+        
+        
     }
 
     void PlayerWalk()
-    {       
-
+    {
         float h = Input.GetAxisRaw("Horizontal");
-        if (h > 0)
+
+        if (Input.GetKeyDown(KeyCode.J))
         {
+
+            anim.Play("PlayerAttack", 0, 0f);
+            print("attack!!");
+            playerAudioData.PlayOneShot(swordclip, 0.5f);
             
+            CheckCollision();
+        }
+        else if (Input.GetKeyUp(KeyCode.J))
+        {
+            anim.SetBool("Attack", false);
+            
+        }
+        else if (h > 0)
+        {
             myBody.velocity = new Vector2(speed, myBody.velocity.y);
             anim.SetBool("isRunning", true);
             ChangeDirection(5);
-           
-
+            
         }
         else if (h < 0)
         {
-            
             myBody.velocity = new Vector2(-speed, myBody.velocity.y);
-            anim.SetBool("isRunning", true);     
+            anim.SetBool("isRunning", true);
             ChangeDirection(-5);
             
-
         }
         else
         {
             myBody.velocity = new Vector2(0F, myBody.velocity.y);
-            anim.SetBool("isRunning", false);          
-            CancelInvoke();
+            anim.SetBool("isRunning", false);
             
+            walking = false;
         }
 
         anim.SetInteger("Speed", Mathf.Abs((int)myBody.velocity.x));
@@ -113,43 +120,86 @@ public class GerPlayerMovement : MonoBehaviour
         transform.localScale = tempScale;
     }
 
-   
 
     void PlayerSounds()
     {
         if (isGrounded)
         {
-            if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
-            {             
-                playerAudioData.PlayOneShot(walkclip, 0.7F);
-            }   
+            if ((Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow)))
+            {
+                walking = true;
+               // print("Walk Audiostart");
+                playerAudioData.loop = true;
+                playerAudioData.clip = walkclip;
+                playerAudioData.Play();
+
+            }
+        }
+        else if (!isGrounded)
+        {
+            //if (jumped ==false)
+            //{
+            //    playerAudioData.Stop();
+            //}
+           
+            if ((Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow)))
+            {
+                walking = true;               
+            }
         }
 
         if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
         {
+
+            //print("Walk Audiostop");
             playerAudioData.Stop();
+            walking = false;
         }
-        
+
+
     }
-
-
-    void CheckIfGrounded()
+        void CheckIfGrounded()
     {
-        isGrounded = Physics2D.Raycast(groundCheckPosition.position, Vector2.down, 0.1f, groundLayer);
+        isGrounded = Physics2D.Raycast(groundCheckPosition.position, Vector2.down, 0.5f, groundLayer);
         //print(isGrounded + "grounded!");
-
+        //fall = false;
         if (isGrounded)
-            
+
         {
             if (jumped)
             {
                 jumped = false;
-                anim.SetBool("Jump", false);  
+                anim.SetBool("Jump", false);
+               
+                //print("Player land");
+                if (walking)
+                {
+              
+                    //print("Land Walk Audiostart");
+                    playerAudioData.loop = true;
+                    playerAudioData.clip = walkclip;
+                    playerAudioData.Play();
+
+                }
+            }
+            if (fall)
+            {
+                
+                print("Player fall and land");
+                //anim.SetBool("Fall", true);
                 playerAudioData.PlayOneShot(landClip, 0.5F);
-                print("Player land");
+                fall = false;
+            }
+            
+        }
+        else if (!isGrounded)
+        {
+         
+            if (!jumped)
+            {
+                fall = true;
             }
         }
-       
     }
 
     void PlayerJump()
@@ -158,16 +208,17 @@ public class GerPlayerMovement : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.Space))
             {
+                playerAudioData.Stop();
                 myBody.velocity = new Vector2(myBody.velocity.x, jumpPower);
-                print("Player jump");
-                
+                print("Player jump");            
                 playerAudioData.PlayOneShot(jumpClip, 0.5F);
                 anim.SetBool("Jump", true);
-
                 StartCoroutine(setJumped());
             }
+
         }
     }
+
 
     void PlayerAttack()
     {
@@ -175,62 +226,43 @@ public class GerPlayerMovement : MonoBehaviour
         {
             anim.SetBool("Attack", true);
             playerAudioData.PlayOneShot(swordclip, 0.5f);
-            print("PlayerAttack");
+            //print("PlayerAttack");
             CheckCollision();
         }
         if (Input.GetKeyUp(KeyCode.J))
         {
             anim.SetBool("Attack", false);
-            print("PlayerStopAttack");
+            // print("PlayerStopAttack");
         }
     }
 
     IEnumerator setJumped()
     {
         yield return new WaitForSeconds(0.5f);
-        print("SET JUMP CO ROUTINE");
+        //print("SET JUMP CO ROUTINE");
         jumped = true;
+        
     }
 
 
     void CheckCollision()
     {
-        print("check collision");
+        //print("check collision");
         //RaycastHit2D leftHit = Physics2D.Raycast(left_Collision.position, Vector2.left, 0.1f, enemyLayer);
         RaycastHit2D SwordHit = Physics2D.Raycast(right_Collision.position, Vector2.right, 0.1f, enemyLayer);
 
-        //Collider2D topHit = Physics2D.OverlapCircle(top_Collision.position, 0.2f, enemyLayer);
-
-        
-        //if (leftHit)
-        //{
-        //    if (leftHit.collider.gameObject.tag == MyTags.PLAYER_TAG)
-        //    {
-     
-        //        // APPLY DAMAGE TO PLAYER
-        //        leftHit.collider.gameObject.GetComponent<PlayerDamage>().DealDamage();
-
-        //    }
-        //}
-
         if (SwordHit)
         {
-            print("hit skeleton!!!");
+            //print("hit skeleton!!!");
             if (SwordHit.collider.gameObject.tag == MyTags.SKELETON_TAG)
             {
                 print("hit skeleton!!!");
-                SwordHit.collider.gameObject.GetComponent<x>().StunSkel();
-                // rightHit.collider.gameObject.GetComponent<PlayerDamage>().DealDamage();
+                SwordHit.collider.gameObject.GetComponent<SkeletonScriptNew>().StunSkel();
+                SwordHit.collider.gameObject.GetComponent<SkeletonScriptNew>().DealDamage();
+                print(SwordHit.collider.gameObject.GetComponent<SkeletonScriptNew>().health--);
 
             }
         }
-
-        //// IF we don't detect collision any more do whats in {}
-        //if (!Physics2D.Raycast(down_Collision.position, Vector2.down, 0.1f))
-        //{
-
-        //    ChangeDirection();
-        //}
 
     }
 
