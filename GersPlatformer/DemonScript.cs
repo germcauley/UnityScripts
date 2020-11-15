b10685d88ab472f19513c955f78f12f0e616e745
germcauley <geraldmcauley@gmail.com>
2020-11-15 21:10:35 +0000

    demonscript

---
+++ GersPlatformer/DemonScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemonScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Hit by player Sword");
+    }
+
+
+
+
+
+
+
+
+
+}//class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
