4187bdb0564f74ccad1cf59a0be8ed201cc53304
germcauley <geraldmcauley@gmail.com>
2020-11-20 20:54:26 +0000

    demonscript started

--- GersPlatformer/DemonScript.cs
+++ GersPlatformer/DemonScript.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class DemonScript : MonoBehaviour
 {
+
+    public Transform parentPrefab;
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        parentPrefab = this.transform.parent;
+        anim = parentPrefab.gameObject.GetComponent<Animator>();
+        anim.SetBool("Attack", false);
     }
 
     // Update is called once per frame
@@ -15,13 +20,25 @@ public class DemonScript : MonoBehaviour
     {
         
     }
+   
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         print("Hit by player Sword");
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            
+            //parentPrefab.gameObject.GetComponent<FixedSkeleton>().canWalk = false;
+            //parentPrefab.gameObject.GetComponent<FixedSkeleton>().attacking = true;
+            //parentPrefab.gameObject.GetComponent<FixedSkeleton>().Attack();
+        }
     }
 
-
+    void Attack()
+    {
+        
+    }
 
 
 
