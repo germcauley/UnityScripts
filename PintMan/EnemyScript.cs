76618fd2440b1f24b1bdb21ed096faf158166641
germcauley <geraldmcauley@gmail.com>
2021-01-07 23:46:56 +0000

    enemy collision

--- PintMan/EnemyScript.cs
+++ PintMan/EnemyScript.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class enemyscript : MonoBehaviour
 {
     public Transform EnemyHeadCollider;
-    public LayerMask Player;
-    private bool isHit;
+    public LayerMask playerLayer;
+     
 
     // Start is called before the first frame update
     void Start()
@@ -18,38 +18,69 @@ public class enemyscript : MonoBehaviour
     void Update()
     {
 
-        CheckIfHit();
-
-        if (Physics2D.Raycast(EnemyHeadCollider.position, Vector2.up, 0.5f, Player))
+        //CheckIfHit();
+        CheckCollision();
+        if (Physics2D.Raycast(EnemyHeadCollider.position, Vector2.up, 0.5f, playerLayer))
         {
             //print("Collided with enemy");
         }
     }
 
 
-    void CheckIfHit()
-    {
-        isHit = Physics2D.Raycast(EnemyHeadCollider.position, Vector2.up, 0.1f, Player);
+    //void CheckIfHit()
+    //{
+    //    isHit = Physics2D.Raycast(EnemyHeadCollider.position, Vector2.up, 0.1f, playerLayer);
 
-        //print("Collided with enemy");
+    //    //print("Collided with enemy");
 
-        if (isHit)
-        {
+    //    if (isHit)
+    //    {
             
-           //print("Collided with enemy");
+    //       print("Collided with enemy");
+    //    }
+    //}
+
+
+
+   // check for player collision using raycast
+    void CheckCollision()
+    {
+        
+
+        //RaycastHit2D leftHit = Physics2D.Raycast(left_Collision.position, Vector2.left, 0.1f, playerLayer);
+        //RaycastHit2D rightHit = Physics2D.Raycast(right_Collision.position, Vector2.right, 0.1f, playerLayer);
+
+        Collider2D topHit = Physics2D.OverlapCircle(EnemyHeadCollider.position, 0.2f, playerLayer);
+        print(topHit.gameObject.name);
+        if (topHit != null)
+        {
+            if (topHit.gameObject.tag == MyTags.PLAYER_TAG)
+            {
+                print("Enemy hit!!!");
+                //if (!stunned)
+                //{
+                topHit.gameObject.GetComponent<Rigidbody2D>().velocity =
+                new Vector2(topHit.gameObject.GetComponent<Rigidbody2D>().velocity.x, 7f);
+
+                //    canMove = false;
+                //    myBody.velocity = new Vector2(0, 0);
+
+                //    anim.Play("Stunned");
+                //    stunned = true;
+
+                //    // BEETLE CODE HERE
+                //    if (tag == MyTags.BEETLE_TAG)
+                //    {
+                //        anim.Play("Stunned");
+                //        StartCoroutine(Dead(0.5f));
+                //    }
+                //}
+            }          
         }
     }
 
 
-    void OnCollisionEnter2D(Collision2D coll)
-    {        
-        if (coll.gameObject.tag == "Player")
-            print("hit enemy");
-
-    }
 
 
 
-
-
-}//class
+    }//class
