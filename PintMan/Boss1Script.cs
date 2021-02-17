5ff1be5e6ce7b51b06bd5bad7210845bc0d13b31
germcauley <geraldmcauley@gmail.com>
2021-02-17 20:39:14 +0000

    boss movement

--- PintMan/Boss1Script.cs
+++ PintMan/Boss1Script.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Boss1Script : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private bool moveLeft = true,canHit=true;
+    public float moveSpeed = 1f;
     void Start()
     {
         
@@ -13,5 +14,55 @@ public class Boss1Script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (moveLeft)
+        {
+            //determines movement of object move X axis only in this case
+            transform.Translate(-2 * Time.deltaTime * moveSpeed, 0, 0);
+            transform.localScale = new Vector2(2.2f, 2.2f);
+            transform.rotation = Quaternion.Euler(-2, 0, -7.552f);
+
+        }
+
+        else if (!moveLeft)
+        {
+            transform.Translate(2 * Time.deltaTime * moveSpeed, 0, 0);
+            transform.localScale = new Vector2(-2.2f, 2.2f);
+            transform.rotation = Quaternion.Euler(2, 0, 7.552f);
+
+
+        }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        //patrol script
+        if (collision.gameObject.CompareTag("PatrolPoint")&& canHit)
+        {
+            canHit = false;
+            StartCoroutine(SwitchDirection());
+        }
+    }
+
+    IEnumerator SwitchDirection()
+    {
+        print("Switch direction!");
+
+        if (moveLeft)
+        {
+            moveLeft = false;
+
+        }
+        else
+        {
+            moveLeft = true;
+
+        }
+        yield return new WaitForSeconds(2.0f);
+        canHit = true;
+    }
+
+
+
+
+}//class
