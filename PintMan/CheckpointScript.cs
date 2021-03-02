51380e68fb0ea4f0680f5c371f6f09f9a17aa1ca
Gerald McAuley <geraldmcauley@gmail.com>
2021-03-02 21:29:46 +0000

    Create CheckpointScript.cs

---
+++ PintMan/CheckpointScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkpoint : MonoBehaviour
+{
+
+
+    private GameMaster gm;
+
+    void Start(){
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+    }
+    void OnTriggerenter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            gm.lastCheckPointPos = transform.position;
+        }
+
+    }
+}
