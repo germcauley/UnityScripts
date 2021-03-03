bfd4dc8dd52194c6c690b967c37b936745b16a50
Gerald McAuley <geraldmcauley@gmail.com>
2021-03-03 18:33:52 +0000

    Update CheckpointScript.cs

--- PintMan/CheckpointScript.cs
+++ PintMan/CheckpointScript.cs
@@ -7,16 +7,36 @@ public class checkpoint : MonoBehaviour
 
 
     private GameMaster gm;
+    public AudioClip CheckPointClip;
+    private AudioSource CheckPointAudio;
+    private Animator myAnimator;
 
     void Start(){
         gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        CheckPointAudio = GetComponent<AudioSource>();     
+        myAnimator = GetComponent<Animator>();
+        myAnimator.GetComponent<Animator>().enabled = false;
+
     }
-    void OnTriggerenter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             gm.lastCheckPointPos = transform.position;
+            CheckPointAudio.PlayOneShot(CheckPointClip, 1f);
+            StartCoroutine(PlayCheckPointAnim());
         }
 
     }
-}
+
+    IEnumerator PlayCheckPointAnim()
+    {
+        myAnimator.GetComponent<Animator>().enabled = true;
+        yield return new WaitForSeconds(.5f);
+
+    }
+        
+        
+       
+
+}//end of class
