8c3af9fafabf840947b9aa1d07e334fa23f73ae1
Gerald McAuley <geraldmcauley@gmail.com>
2021-03-05 19:52:20 +0000

    Update MrSpudScript.cs

--- PintMan/MrSpudScript.cs
+++ PintMan/MrSpudScript.cs
@@ -7,19 +7,23 @@ public class MrSpudScript : MonoBehaviour
     public GameObject bagOfSpuds, Pintman;
     public Transform attackInstantiate;
     private Animator anim;
-    private bool stunned = false;
+    private bool stunned = false,hit=false;
     public LayerMask playerLayer;
     private string coroutine_Name = "StartAttack";
     public float ProjectileSpeed, fireRate, nextFire;
     public AudioClip BossPainClip;
     private AudioSource BossAudioData;
     public Transform EnemyHeadCollider;
+    private Rigidbody2D pintManRB;
+    public Vector2 movement;
 
 
     void Start()
     {
         Pintman = GameObject.Find("PintMan");
         BossAudioData = gameObject.GetComponent<AudioSource>();
+        anim = gameObject.GetComponent<Animator>();
+        pintManRB = Pintman.GetComponent<Rigidbody2D>();
     }
 
 
@@ -32,20 +36,32 @@ public class MrSpudScript : MonoBehaviour
             //print("Collided with enemy");
         }
         //Makes the Enemy sprite face the direction of the player character byt flipping x axis when required
-        //if (transform.position.x > Pintman.transform.position.x)
-        //{
-        //    transform.localScale = new Vector2(-0.15f, 0.15f);
-        //}
-        //else if (transform.position.x < Pintman.transform.position.x)
-        //{
-        //    transform.localScale = new Vector2(0.15f, 0.15f);
-        //}
+        if (transform.position.x > Pintman.transform.position.x)
+        {
+            transform.localScale = new Vector2(2f, 2f);
+        }
+        else if (transform.position.x < Pintman.transform.position.x)
+        {
+            transform.localScale = new Vector2(-2f, 2f);
+        }
 
-        //if (GardaHealth <= 0)
-        //{
-        //    StartCoroutine(DestroyEnemy());
-        //}
+       
+        movement = new Vector2(100f, 0f);
     }
+
+    private void FixedUpdate()
+    {
+        if (hit)
+        {
+            moveCharacter(movement);
+        }
+    }
+
+    void moveCharacter(Vector2 direction)
+    {
+        pintManRB.AddForce(direction * 10f);
+    }
+
 
     void Attack()
     {
@@ -67,20 +83,18 @@ public class MrSpudScript : MonoBehaviour
     {
 
 
-        Collider2D topHit = Physics2D.OverlapCircle(EnemyHeadCollider.position, 0.2f, playerLayer);
+        Collider2D topHit = Physics2D.OverlapCircle(EnemyHeadCollider.position, 0.5f, playerLayer);
         //if player jumps on enemy head it can stun or kill
         if (topHit != null)
         {
             if (topHit.gameObject.tag == MyTags.PLAYER_TAG)
-            {
-                print("YOU JUMPED ON MR SPUDS HEAD!!!!!");
-
+            {             
                 if (!stunned)
-                {
-                    BossAudioData.PlayOneShot(BossPainClip, 0.8f);
-                    topHit.gameObject.GetComponent<Rigidbody2D>().velocity =
-                new Vector2(topHit.gameObject.GetComponent<Rigidbody2D>().velocity.x, 7f);
-                    //StartCoroutine(DestroyEnemy());
+                {                
+                    stunned = true;
+                    hit = true;
+                    print("BOSS is INVINCIBLE");
+                    StartCoroutine(Stunned());                   
                 }
             }
         }
@@ -89,11 +103,19 @@ public class MrSpudScript : MonoBehaviour
     IEnumerator StartAttack()
     {
         yield return new WaitForSeconds(Random.Range(2f, 5f));
-
         anim.Play("BossAttack");
         StartCoroutine(coroutine_Name);
     }
 
 
-
+    IEnumerator Stunned()
+    {
+        BossAudioData.PlayOneShot(BossPainClip, 0.8f);
+        anim.SetBool("Stunned", true);        
+        yield return new WaitForSeconds(1.5f);
+        anim.SetBool("Stunned", false);
+        stunned = false;
+        print("BOSS NO LONGER INVINCIBLE");
+    }
+   
 }
